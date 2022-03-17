5a6513384eb99f16f6a94dd2164d11f68ec5fd7a
xutengrui_1993 <xutr@yoozoo.com>
2022-03-17 14:03:58 +0800

    restful-api

---
+++ seatunnel-restful-service/src/test/java/org/apache/seatunnel/seatunnelrestfulservice/SeatunnelRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.seatunnel.seatunnelrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatunnelRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
